1ad4616720e8ca0f10d24816cee2c80bc79df5d4
mudkipmaster <acg59@drexel.edu>
2019-03-18 15:53:07 -0400

    fixed the title and coloring

--- src/app/models/HSLColor.ts
+++ src/app/models/HSLColor.ts
@@ -15,11 +15,11 @@ export class HSLColor {
 
   static getLightened(color: HSLColor) {
     // bound the transform so that it only produces valid colors
-    return new HSLColor(color.hue, Math.max(color.saturation - 15, 0), Math.min(color.lightness + 15, 100));
+    return new HSLColor(color.hue, Math.max(color.saturation - 15, 0), Math.min(color.lightness + 10, 90));
   }
 
   static getDarkened(color: HSLColor) {
-    return new HSLColor(color.hue, Math.min(color.saturation + 15, 100), Math.max(color.lightness - 15, 0));
+    return new HSLColor(color.hue, Math.min(color.saturation + 15, 100), Math.max(color.lightness - 15, 50));
   }
 
 }
